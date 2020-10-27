2990f567591bfc198b4d9ffd756128df3eb36417
Huynh Gia Boi <giaboi.hito@gmail.com>
2020-10-27 13:46:50 +0700

    New commit

--- src/UI_Components/PopupSettingComponent.js
+++ src/UI_Components/PopupSettingComponent.js
@@ -3,24 +3,39 @@ import PropTypes from 'prop-types';
 import Popup from 'reactjs-popup';
 import { Context } from './../Context/context';
 import { closeSetting } from './../Context/actions';
-import { Container, Row, Col } from 'react-bootstrap';
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  Card,
+  Button,
+  Modal,
+} from 'react-bootstrap';
 
 function PopupSettingComponent(props) {
   const {} = props;
   const [state, dispatch] = useContext(Context);
 
+  const handleClose = () => {
+    dispatch(closeSetting());
+  };
+
   return (
-    <Popup
-      open={state.isOpenSetting}
-      modal
-      onClose={() => dispatch(closeSetting())}
-    >
-      <Container>
-        <Row>
-          <Col>1 of 1</Col>
-        </Row>
-      </Container>
-    </Popup>
+    <Modal show={state.isOpenSetting} onHide={handleClose} centered>
+      <Modal.Header closeButton>
+        <Modal.Title>Setting</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+        <Button variant="primary" onClick={handleClose}>
+          Save Changes
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
 }
 
